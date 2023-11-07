b521b091f0857a7ff8e42d44e24e59de687d6b5a
Jimmy Yue <53287360+jimmyue@users.noreply.github.com>
2023-11-07 16:29:05 +0800

    Update run.py
    1

--- run.py
+++ run.py
@@ -8,6 +8,22 @@ import random
 import pymysql
 from faker import Faker
 
+# CREATE TABLE `tb_personal_information` (
+#   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+#   `name` varchar(50) DEFAULT NULL COMMENT '姓名',
+#   `gender` varchar(10) DEFAULT NULL COMMENT '性别',
+#   `age` varchar(10) DEFAULT NULL COMMENT '年龄',
+#   `address` varchar(100) DEFAULT NULL COMMENT '详细地址',
+#   `province` varchar(10) DEFAULT NULL COMMENT '省份',
+#   `city` varchar(10) DEFAULT NULL COMMENT '城市',
+#   `phone` varchar(50) DEFAULT NULL COMMENT '手机号',
+#   `carid` varchar(50) DEFAULT NULL COMMENT '身份证号',
+#   `birthday` varchar(10) DEFAULT NULL COMMENT '出生年月',
+#   `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+#   `cdate` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+#   PRIMARY KEY (`id`)
+# )
+
 def data_qury(db,sql):
     '''
     数据库查询
@@ -38,7 +54,7 @@ def get_data():
     '''
     key_list = ["姓名","性别","年龄","详细地址","省份","城市","手机号","身份证号","出生年月","邮箱","创建时间"]
     name = fake.name()
-    gender= random.choice(["男","女"])
+    gender= random.choice(["男","女","NULL"])
     age=fake.random_int(min=18,max=50)
     address = fake.address()
     province = fake.province()
@@ -53,13 +69,21 @@ def get_data():
     return person_info
     
 #打开数据库连接
-db=pymysql.connect(host="10.10.10.71",user="root",passwd="123456",db="jimmy",port=3306,use_unicode=True, charset="utf8")
+db=pymysql.connect(host="xxxx",user="jimmy",passwd="xxx",db="jimmy",port=3306,use_unicode=True, charset="utf8")
 #插入测试数据
 fake = Faker(locale='zh_CN')
-for i in range(10):
+n=10 #修改需要插入的数据行数
+for i in range(n):
     data=get_data()
-    sql = "INSERT INTO personal_information(name,gender,age,address,province,city,phone,carid,birthday,email,cdate) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (data['姓名'],data['性别'],data['年龄'],data['详细地址'],data['省份'],data['城市'],data['手机号'],data['身份证号'],data['出生年月'],data['邮箱'],data['创建时间'])
+    if data['性别']=="NULL": #插入NULL值
+        sql = "INSERT INTO tb_personal_information(name,gender,age,address,province,city,phone,carid,birthday,email,cdate) \
+        VALUES ('%s',%s,'%s','%s','%s','%s','%s','%s','%s','%s','%s')" % \
+        (data['姓名'],data['性别'],data['年龄'],data['详细地址'],data['省份'],data['城市'],data['手机号'],data['身份证号'],data['出生年月'],data['邮箱'],data['创建时间'])
+    else:
+        sql = "INSERT INTO tb_personal_information(name,gender,age,address,province,city,phone,carid,birthday,email,cdate) \
+        VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')" % \
+        (data['姓名'],data['性别'],data['年龄'],data['详细地址'],data['省份'],data['城市'],data['手机号'],data['身份证号'],data['出生年月'],data['邮箱'],data['创建时间'])
     data_alter(db,sql)
-print('插入数据完成！')
+print('插入%s行数据成功！'%n)
 # 关闭数据库连接
 db.close()
